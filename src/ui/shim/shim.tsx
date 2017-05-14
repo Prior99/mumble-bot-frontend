ad8b934e66c51d54de7c71d4c74efda59db4647c
Frederick <frederick@gnodtke.net>
2017-05-14 13:53:06 +0200

    Add Shim

---
+++ src/ui/shim/shim.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import ProgressBar from "react-toolbox/lib/progress_bar";
+import { observer, inject } from "mobx-react";
+import { observable, action } from "mobx";
+import * as style from "./style.scss";
+import { RecordingsState, UsersState, isLoading } from "../../store";
+
+interface ShimProps {
+    users?: UsersState;
+    recordings?: RecordingsState;
+}
+
+@inject("users", "recordings")
+@observer
+export class Shim extends React.Component<ShimProps, undefined> {
+    render() {
+        const { users, recordings } = this.props;
+        if (!isLoading(users, recordings)) {
+            return <div />;
+        }
+        return (
+            <div className={style.shim}>
+                <div className={style.content}>
+                    <p>Loading...</p>
+                    <ProgressBar mode="indeterminate" />
+                </div>
+            </div>
+        );
+    }
+}
