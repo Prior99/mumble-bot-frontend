3c7b07eb86f7654eb8a1a3e9d2bacb15b509280d
Frederick <frederick@gnodtke.net>
2017-05-17 19:23:42 +0200

    More beautiful labels

--- src/ui/label/label.tsx
+++ src/ui/label/label.tsx
@@ -5,24 +5,32 @@ import * as style from "./style.scss";
 import { LabelsState } from "../../store";
 import { colorize } from "../../utils";
 
+interface LabelComponentProps {
+    id: number;
+    labels?: LabelsState;
+    amount?: boolean;
+}
+
 @inject("labels")
 @observer
-export class LabelComponent extends React.Component<{ id: number, labels?: LabelsState }, undefined> {
+export class LabelComponent extends React.Component<LabelComponentProps, undefined> {
     public render() {
-        const { id, labels } = this.props;
+        const { id, labels, amount } = this.props;
         const label = labels.getLabel(id);
         const { recordings, name } = label;
         const relevance = labels.getRelevance(label);
         const paleColor = colorize(name, 1, 0.8 + (1.0 - relevance) * 0.2);
-        const strongColor = colorize(name, 1, 0.5 + (1.0 - relevance) * 0.2);
-        const nameColor = colorize(name, 0.8, 0.5);
-        const amountColor = colorize(name, 0.8, 0.9);
+        const extraPaleColor = colorize(name, 1, 0.9 + (1.0 - relevance) * 0.1);
+        const nameColor = colorize(name, 0.5, 0.4);
         return (
-            <div className={style.label} style={{ background: paleColor, border: `1px solid ${strongColor}` }}>
-                <div className={style.amount} style={{ background: strongColor, color: amountColor }}>
-                    {recordings}
-                </div>
-                <div className={style.name} style={{ color: nameColor }}>
+            <div className={style.label} style={{ background: paleColor, boxShadow: `${extraPaleColor} 0px 0px 5px 2px` }}>
+                {
+                    amount &&
+                    <div className={style.amount} style={{ background: extraPaleColor, color: nameColor }}>
+                        {recordings}
+                    </div>
+                }
+                <div className={style.name} style={{ color: nameColor, marginLeft: !amount && "10px" }}>
                     {name}
                 </div>
             </div>
